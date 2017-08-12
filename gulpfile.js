a203c3285e0d33d4e650a8e6661cc75788979420
wawjr3d <wawjr3d@gmail.com>
2017-08-12 00:32:16 -0400

    upgrade things, merge with wawjr3d

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
 'use strict';
 
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
 
 gulp.task('stylesheet', function() {
   return gulp.src('./urldecoder.scss')
     .pipe(sass())
+    .pipe(cleanCSS({ debug: true }, function(details) {
+      console.log(`${ details.name }: ${ details.stats.originalSize } to ${ details.stats.minifiedSize }`);
+    }))
     .pipe(gulp.dest('./build'));
 });
 
 gulp.task('javascript', function() {
-  return gulp.src('urldecoder.js')
-    .pipe(browserify({
-      debug: false
-    }))
+  return browserify('urldecoder.js')
+    .bundle()
+    .pipe(source('urldecoder.js'))
+    .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest('./build'));
 });
 
